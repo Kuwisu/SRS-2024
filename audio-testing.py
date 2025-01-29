b223a65544a515bf73ed92eaf52fc72589e7927e
Kuwisu <ckolle.jensen@gmail.com>
2025-01-29 23:46:20 +1300

    exploring alternatives to librosa stft in a separate file

--- audio-testing.py
+++ audio-testing.py
@@ -4,6 +4,10 @@ import soxr
 import librosa
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.mlab import window_hanning
+from matplotlib.ticker import ScalarFormatter
+from scipy.signal.windows import *
+from scipy.signal import ShortTimeFFT
 
 # %% Read function using soundfile vs using Librosa
 data, samplerate = sf.read('playback.wav', dtype='float32', always_2d=True)
@@ -30,11 +34,54 @@ print(len(y2))
 # print(x2)
 # print(len(x2))
 
-# %% Plotting using matplotlib vs waveshow
+# %% Prepare plot
 fig, ax = plt.subplots(nrows=2)
+
+# %% Plotting using plt.step vs waveshow
 times = np.linspace(0, len(y2) / 22050, len(y2), dtype='float32')
+# ax[0].step(times, y2, where='post')
 
-ax[0].step(times, y2, where='post')
-librosa.display.waveshow(y2, sr=22050, ax=ax[1], where='post')
+# librosa.display.waveshow(y2, sr=22050, ax=ax[1], where='post')
+
+# %% Plotting a linear spectrogram using librosa vs scipy, both using librosa.stft
+# Librosa spectrogram
+s = librosa.stft(y2, n_fft=512, hop_length=128, win_length=512, window='hann')
+s_db = librosa.amplitude_to_db(np.abs(s), ref=np.max)
+img = librosa.display.specshow(s_db, sr=22050, n_fft=512, hop_length=128, win_length=512,
+                               ax=ax[0], cmap='viridis', x_axis='time', y_axis='linear')
+plt.colorbar(img, ax=ax[0])
+
+# Scipy spectrogram
+w = hann(512)
+SFT = ShortTimeFFT(win=w, hop=128, fs=22050, fft_mode='onesided', mfft=512)
+Sx = SFT.spectrogram(y2)
+Sx_db = 10 * np.log10(np.fmax(Sx, 1e-5)) # range is limited to -50 dB
+im1 = ax[1].imshow(Sx_db, origin='lower', aspect='auto', cmap='viridis',
+                   extent=SFT.extent(len(y2)))
+plt.colorbar(im1, ax=ax[1])
+
+# %% Plot the same but with a log magnitude spectrogram
+# # Librosa spectrogram
+# # s = librosa.stft(y2, n_fft=512, hop_length=128, win_length=512, window='hann')
+# # s_db = librosa.amplitude_to_db(np.abs(s), ref=np.max)
+# img = librosa.display.specshow(s_db, sr=16000, n_fft=512, hop_length=128, win_length=512,
+#                                ax=ax[0], cmap='viridis', x_axis='time', y_axis='log')
+# plt.colorbar(img, ax=ax[0])
+#
+# # Scipy spectrogram
+# w = hann(512)
+# SFT = ShortTimeFFT(win=w, hop=128, fs=16000, fft_mode='onesided', mfft=512)
+# Sx = SFT.spectrogram(y2)
+# Sx_db = 10 * np.log10(np.fmax(Sx, 1e-5)) # range is limited to -50 dB
+# im1 = ax[1].imshow(Sx_db, origin='lower', aspect='auto', cmap='viridis',
+#                    extent=SFT.extent(len(y2)))
+# ticks = ax[1].get_yticks()
+# plt.yscale('symlog')
+# plt.ylim(100, int(16000/2))
+# ax[1].yaxis.set_major_formatter(ScalarFormatter())
+# # ax[1].set_yticks(ticks)
+# plt.colorbar(im1, ax=ax[1])
+
+
+# %% Display plot
 plt.show()
-
