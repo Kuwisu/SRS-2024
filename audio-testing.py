found the correct amplitude to db calculation

audio-testing.py
@@ -8,6 +8,7 @@ from matplotlib.mlab import window_hanning
 from matplotlib.ticker import ScalarFormatter
 from scipy.signal.windows import *
 from scipy.signal import ShortTimeFFT
+from scipy.signal import spectrogram
 
 # %% Read function using soundfile vs using Librosa
 data, samplerate = sf.read('playback.wav', dtype='float32', always_2d=True)
@@ -47,41 +48,39 @@ times = np.linspace(0, len(y2) / 22050, len(y2), dtype='float32')
 # Librosa spectrogram
 s = librosa.stft(y2, n_fft=512, hop_length=128, win_length=512, window='hann')
 s_db = librosa.amplitude_to_db(np.abs(s), ref=np.max)
-img = librosa.display.specshow(s_db, sr=22050, n_fft=512, hop_length=128, win_length=512,
-                               ax=ax[0], cmap='viridis', x_axis='time', y_axis='linear')
-plt.colorbar(img, ax=ax[0])
+# img = librosa.display.specshow(s_db, sr=22050, n_fft=512, hop_length=128, win_length=512,
+#                                ax=ax[0], cmap='viridis', x_axis='time', y_axis='linear')
+# plt.colorbar(img, ax=ax[0])
 
 # Scipy spectrogram
 w = hann(512)
 SFT = ShortTimeFFT(win=w, hop=128, fs=22050, fft_mode='onesided', mfft=512)
 Sx = SFT.spectrogram(y2)
-Sx_db = 10 * np.log10(np.fmax(Sx, 1e-5)) # range is limited to -50 dB
-im1 = ax[1].imshow(Sx_db, origin='lower', aspect='auto', cmap='viridis',
-                   extent=SFT.extent(len(y2)))
-plt.colorbar(im1, ax=ax[1])
-
-# %% Plot the same but with a log magnitude spectrogram
-# # Librosa spectrogram
-# # s = librosa.stft(y2, n_fft=512, hop_length=128, win_length=512, window='hann')
-# # s_db = librosa.amplitude_to_db(np.abs(s), ref=np.max)
-# img = librosa.display.specshow(s_db, sr=16000, n_fft=512, hop_length=128, win_length=512,
-#                                ax=ax[0], cmap='viridis', x_axis='time', y_axis='log')
-# plt.colorbar(img, ax=ax[0])
-#
-# # Scipy spectrogram
-# w = hann(512)
-# SFT = ShortTimeFFT(win=w, hop=128, fs=16000, fft_mode='onesided', mfft=512)
-# Sx = SFT.spectrogram(y2)
-# Sx_db = 10 * np.log10(np.fmax(Sx, 1e-5)) # range is limited to -50 dB
+Sx_db = 10 * np.log10(np.fmax(Sx, 1e-5)) - 30
 # im1 = ax[1].imshow(Sx_db, origin='lower', aspect='auto', cmap='viridis',
 #                    extent=SFT.extent(len(y2)))
-# ticks = ax[1].get_yticks()
-# plt.yscale('symlog')
-# plt.ylim(100, int(16000/2))
-# ax[1].yaxis.set_major_formatter(ScalarFormatter())
-# # ax[1].set_yticks(ticks)
 # plt.colorbar(im1, ax=ax[1])
 
+# %% Plot the same but with a log magnitude spectrogram
+# Librosa spectrogram
+img = librosa.display.specshow(s_db, sr=22050, n_fft=512, hop_length=128, win_length=512,
+                               ax=ax[0], cmap='viridis', x_axis='time', y_axis='log')
+plt.colorbar(img, ax=ax[0])
+
+# Scipy spectrogram
+im1 = ax[1].imshow(Sx_db, origin='lower', aspect='auto', cmap='viridis',
+                   extent=SFT.extent(len(y2)))
+ticks = []
+tick = 64
+while tick < int(22050/2):
+    ticks.append(tick)
+    tick = tick * 2
+
+plt.yscale('symlog')
+plt.yticks(ticks)
+plt.ylim(32, int(22050/2))
+ax[1].yaxis.set_major_formatter(ScalarFormatter())
+plt.colorbar(im1, ax=ax[1])
 
 # %% Display plot
 plt.show()

main.py
@@ -163,11 +163,11 @@ class AudioTool:
             s_db = librosa.amplitude_to_db(np.abs(s), ref=np.max)
 
         # Show the spectrogram as a graph with a colour bar to view z axis info
-        self.img = librosa.display.specshow(s_db, sr=self.sr,
+        img = librosa.display.specshow(s_db, sr=self.sr,
                                             n_fft=n_fft, win_length=win_length,
                                             hop_length=hop_length, cmap=cmap,
                                             ax=self.ax[1], x_axis='time', y_axis=scale)
-        self.colorbar = self.figure.colorbar(self.img, ax=self.ax[1], format="%+2.f dB")
+        self.colorbar = self.figure.colorbar(img, ax=self.ax[1], format="%+2.f dB")
         self.ax[1].set_title(f'{scale} spectrogram with {win_length} window length')
         self.ax[1].label_outer()
 